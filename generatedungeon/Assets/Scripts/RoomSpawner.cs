57ef2f9eb088a9148c9ba2a28981158e88773a62
BrandonC <brandoncoenraads@gmail.com>
2019-11-04 17:56:54 +0100

    spawners

---
+++ generatedungeon/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+
+    public int openingDirection;
+    // 1 = bot
+    // 2 = top
+    // 3 = left
+    // 4 = right
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (openingDirection == 1)
+        {
+
+        } else if (openingDirection == 2)
+        {
+
+        } else if (openingDirection == 3)
+        {
+
+        } else if (openingDirection == 4)
+        {
+
+        }
+    }
+}
