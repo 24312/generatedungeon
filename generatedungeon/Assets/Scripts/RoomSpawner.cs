30cd1eadb1c45396d4beff562e13c32037f9ad84
BrandonC <brandoncoenraads@gmail.com>
2019-11-05 13:36:21 +0100

    first working prototype

--- generatedungeon/Assets/Scripts/RoomSpawner.cs
+++ generatedungeon/Assets/Scripts/RoomSpawner.cs
@@ -11,27 +11,50 @@ public class RoomSpawner : MonoBehaviour
     // 3 = left
     // 4 = right
 
+    private Roomtemplates Templates;
+    private int Rand;
+    private bool Spawned = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<Roomtemplates>();
+        Invoke("Spawn", 0.1f);
     }
 
     // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        if (openingDirection == 1)
-        {
+        if (Spawned == false){
+            if (openingDirection == 1)
+            {
+                Rand = Random.Range(0, Templates.BotRooms.Length);
+                Instantiate(Templates.BotRooms[Rand], transform.position, Templates.BotRooms[Rand].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                Rand = Random.Range(0, Templates.TopRooms.Length);
+                Instantiate(Templates.TopRooms[Rand], transform.position, Templates.TopRooms[Rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                Rand = Random.Range(0, Templates.LeftRooms.Length);
+                Instantiate(Templates.LeftRooms[Rand], transform.position, Templates.LeftRooms[Rand].transform.rotation);
 
-        } else if (openingDirection == 2)
+            }
+            else if (openingDirection == 4)
+            {
+                Rand = Random.Range(0, Templates.RightRooms.Length);
+                Instantiate(Templates.RightRooms[Rand], transform.position, Templates.RightRooms[Rand].transform.rotation);
+            }
+            Spawned = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Spawnpoint"))
         {
-
-        } else if (openingDirection == 3)
-        {
-
-        } else if (openingDirection == 4)
-        {
-
+            Destroy(gameObject);
         }
     }
 }
